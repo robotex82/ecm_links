fce37505fca832fb6359b3379b606980ef49d402
Roberto Vasquez Angel <robotex@robotex.de>
2012-09-13 14:36:00 +0200

    Moved active admin files to lib.

--- lib/generators/ecm/links/locales/locales_generator.rb
+++ lib/generators/ecm/links/locales/locales_generator.rb
@@ -7,8 +7,12 @@ module Ecm
         source_root File.expand_path('../../../../../../config/locales', __FILE__)
         
         def generate_locales
+          copy_file "ecm.links.category.en.yml", "config/locales/ecm.links.category.en.yml"
+          copy_file "ecm.links.category.de.yml", "config/locales/ecm.links.category.de.yml"
           copy_file "ecm.links.en.yml", "config/locales/ecm.links.en.yml"
           copy_file "ecm.links.de.yml", "config/locales/ecm.links.de.yml"
+          copy_file "ecm.links.link.en.yml", "config/locales/ecm.links.link.en.yml"
+          copy_file "ecm.links.link.de.yml", "config/locales/ecm.links.link.de.yml"
         end   
       end
     end
