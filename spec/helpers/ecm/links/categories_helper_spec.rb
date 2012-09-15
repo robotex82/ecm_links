a1bab0fce7e446f795956913799f8f568502f462
Roberto Vasquez Angel <robotex@robotex.de>
2012-09-15 16:39:22 +0200

    Better specs.

--- spec/helpers/ecm/links/categories_helper_spec.rb
+++ spec/helpers/ecm/links/categories_helper_spec.rb
@@ -4,25 +4,13 @@ module Ecm
   module Links
     describe CategoriesHelper do    
       describe "#render_link_category" do
-        context "basic rendering" do
-          before(:each) do
-            I18n.locale = :en
-            @category = FactoryGirl.create(:ecm_links_category, :locale => 'en')
-          end
-          
-          subject { helper.render_link_category(@category) }
-
-          it { should have_content(@category.name) }          
+        before(:each) do
+          @category = FactoryGirl.create(:ecm_links_category)
         end
+        
+        subject { helper.render_link_category(@category) }
 
-        context "i18n support" do
-          before(:each) do
-            I18n.locale = :en
-            @category = FactoryGirl.create(:ecm_links_category, :locale => 'de')
-          end
-
-          it { should_not have_content(@category.name) }  
-        end
+        it { should have_content(@category.name) }          
       end
     end
   end
--- spec/helpers/ecm/links_helper_spec.rb
+++ spec/helpers/ecm/links_helper_spec.rb
@@ -4,7 +4,8 @@ module Ecm
   describe LinksHelper do    
     describe "#render_link_footer" do
       before(:each) do
-        @categories = FactoryGirl.create_list(:ecm_links_category_for_link_footer, 6)
+        I18n.locale = :en
+        @categories = FactoryGirl.create_list(:ecm_links_category_for_link_footer, 6, :locale => :en)
       end
       
       subject { helper.render_link_footer }
@@ -12,6 +13,26 @@ module Ecm
       it "should show all category names" do
         @categories.each do |category|
           subject.should have_content(category.name)
+        end
+      end
+      
+      context "i18n support" do
+        before(:each) do
+          I18n.locale = :de
+          @de_categories = FactoryGirl.create_list(:ecm_links_category_for_link_footer, 6, :locale => :de)
+          @en_categories = FactoryGirl.create_list(:ecm_links_category_for_link_footer, 6, :locale => :en)
+        end
+        
+        it "should show category names with the actual locale" do
+          @de_categories.each do |category|
+            subject.should have_content(category.name)
+          end
+        end
+        
+        it "should not show category names with a different locale" do
+          @en_categories.each do |category|
+            subject.should_not have_content(category.name)
+          end
         end
       end
     end
